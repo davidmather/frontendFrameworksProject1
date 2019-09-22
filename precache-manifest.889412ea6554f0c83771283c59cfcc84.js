2e2751628ad95ae530cee245b0cd59b51e083005
David George Mather <davidmather@live.ie>
2019-09-22 03:33:02 +0100

    Updates

---
+++ precache-manifest.889412ea6554f0c83771283c59cfcc84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79772747c29cf3ddfaba0afbea76aa6",
+    "url": "/frontendFrameworksProject1/index.html"
+  },
+  {
+    "revision": "dbd5da8638dfe3060e6a",
+    "url": "/frontendFrameworksProject1/static/css/2.8e17aeb2.chunk.css"
+  },
+  {
+    "revision": "5b386197c15c4dad7751",
+    "url": "/frontendFrameworksProject1/static/css/main.666e8b89.chunk.css"
+  },
+  {
+    "revision": "dbd5da8638dfe3060e6a",
+    "url": "/frontendFrameworksProject1/static/js/2.7c8a49c8.chunk.js"
+  },
+  {
+    "revision": "5b386197c15c4dad7751",
+    "url": "/frontendFrameworksProject1/static/js/main.12656ee4.chunk.js"
+  },
+  {
+    "revision": "86d726bca70c0e550e3d",
+    "url": "/frontendFrameworksProject1/static/js/runtime~main.41e047b9.js"
+  },
+  {
+    "revision": "43687e533d7748cfcc6a91b1ac5175e6",
+    "url": "/frontendFrameworksProject1/static/media/bg-masthead.43687e53.jpg"
+  },
+  {
+    "revision": "8e3e274edccb96b13210c3d5baaad67a",
+    "url": "/frontendFrameworksProject1/static/media/sprites.8e3e274e.png"
+  }
+]);
